e25b0443a8c92eb3f7f35c24a2876105a3c233e7
Korek302 <jacek_blady@interia.pl>
2016-04-11 23:03:23 +0200

    create

---
+++ src/maps/Map.java
@@ -0,0 +1,12 @@
+package maps;
+
+public interface Map 
+{
+	int get(String key);
+	void put(String key , int value);
+	boolean containsKey(String key);
+	int remove(String key);
+	void clear();
+	int size();
+	boolean isEmpty();
+	}
